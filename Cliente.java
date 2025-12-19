7855cedb2c0321191d346c490d0362defda7cc50
Bruno <164259682+BrunoAmorim0003@users.noreply.github.com>
2025-12-19 11:38:15 -0300

    Criação Cliente e Servidor Simples

--- Cliente.java
+++ Cliente.java
@@ -1,4 +1,25 @@
+import java.net.Socket;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
 public class Cliente {
-    public static void main(String[] args) {}
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("localhost", 5000); 
+
+    BufferedReader Teclado = new BufferedReader(new InputStreamReader(System.in)); 
+    PrintWriter Saida = new PrintWriter(socket.getOutputStream(), true); 
+    BufferedReader Entrada = new BufferedReader(new InputStreamReader(socket.getInputStream())); //-Retorna um fluxo de entrada para esse soquete.-//
+
+    System.out.print("Digite uma mensagem: "); 
+    String msg = Teclado.readLine(); 
+    Saida.println(msg); 
+
+    String resposta = Entrada.readLine(); 
+    System.out.println("Resposta do servidor: " + resposta); 
+    socket.close();
+
+    }
 }
-        
+    
--- Server.java
+++ Server.java
@@ -1,4 +1,30 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
 public class Server{
-    public static void main(String[] args) {}
-    int porta = 5000;
+    public static void main(String[] args) throws IOException{
+
+        ServerSocket SocketServer = new ServerSocket(5000);
+        System.out.println("Conexão com Servidor estabelecida...");
+
+        while(true){
+
+            Socket Cliente = SocketServer.accept();
+            System.out.println("Cliente conectado: " + Cliente.getInetAddress());  //-Retorna o endereço ao qual o soquete está conectado-//
+            BufferedReader Entrada = new BufferedReader(new InputStreamReader(Cliente.getInputStream())); 
+            PrintWriter Saida = new PrintWriter(Cliente.getOutputStream(), true); 
+
+            String Mensagem = Entrada.readLine(); 
+            System.out.println("Recebido: " + Mensagem); 
+            
+            Saida.println("Servidor recebeu: " + Mensagem); 
+            Cliente.close();
+        }
+
+    }
+    
 }
